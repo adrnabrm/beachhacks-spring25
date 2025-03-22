60ccd485959e003f8b1314adf685ff747a2de0ee
KinokoLu <myluwork004@gmail.com>
2025-03-22 11:33:28 -0700

    set up backend

---
+++ my-app/backend/routes/api.js
@@ -0,0 +1,9 @@
+// routes/api.js
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({ message: 'Hello from the API route!' });
+});
+
+module.exports = router;
